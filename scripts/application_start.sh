6e347d6dbe00d702e04b99326c1520c23cde72c8
integraldx <integraldx@integraldx.me>
2023-07-17 21:59:29 +0900

    Try other path for docker compose

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo /usr/bin/docker-compose -f /home/ec2-user/misskey-deployment/docker-compose.yml up
+sudo /usr/local/bin/docker-compose -f /home/ec2-user/misskey-deployment/docker-compose.yml up
