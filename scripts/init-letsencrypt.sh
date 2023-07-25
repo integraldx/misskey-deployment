05efbe5b2d1f9bbaa53534c4389f5b6c6c508464
integraldx <integraldx@integraldx.me>
2023-07-25 14:08:13 +0900

    Use absolute path in init script

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(integraldx.social)
 rsa_key_size=4096
-data_path="$HOME/data/certbot"
+data_path="/root/data/certbot"
 email="integraldx@integraldx.dev" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
