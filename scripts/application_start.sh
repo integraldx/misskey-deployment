5194684d4db3305cbbe3fdc578ec0606b4b1404f
integraldx <integraldx@integraldx.me>
2023-07-25 13:59:30 +0900

    Added docker build

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-./init-letsencrypt.sh
+sudo docker-compose build -f /root/misskey-deployment/docker-compose.yml
+sudo ./init-letsencrypt.sh
 sudo docker-compose -f /root/misskey-deployment/docker-compose.yml up -d
