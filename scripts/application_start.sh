97f013ac88d2188abedc7319f9044e6188788cde
Integraldx <integraldx@integraldx.me>
2023-07-30 03:05:52 +0900

    Don't do cert initialization

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 sudo docker-compose build -f /root/misskey-deployment/docker-compose.yml
-source ./init-letsencrypt.sh
 sudo docker-compose -f /root/misskey-deployment/docker-compose.yml up -d
